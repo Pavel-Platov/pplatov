ddc0634a9e4d4346578b2248475bfd1602e228bc
Pavel-Platov <platov_p@mail.ru>
2017-04-04 09:16:30 +0300

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,4 +1,13 @@
 package ru.job4j;
+
+/**
+Package for calculate task.
+
+@author Pavel Platov (platov_p@mail.ru)
+@version $ld$
+@since 0.1
+*/
+
 public class Calculate {
     public static void main(String [] args) {
         System.out.println("Hello world.");
