4d0e86e264fcde072d24779e44a5446976c1fb5c
Pavel-Platov <platov_p@mail.ru>
2017-04-09 20:13:19 +0300

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,15 +1,18 @@
 package ru.job4j;
 
 /**
-*Class for calculate task.
+Class for greeting task.
 
 @author Pavel Platov (platov_p@mail.ru)
 @version $ld$
 @since 04.04.2017
-@param Hello world - method println output Hello world
 */
 
 public class Calculate {
+
+/**
+@param Hello world - method println output Hello world
+*/
     public static void main(String [] args) {
         System.out.println("Hello world.");
       }
