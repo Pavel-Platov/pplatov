c342d6b20b2ab57e9d6c980f62ae99449c6e56d0
Pavel-Platov <platov_p@mail.ru>
2017-04-24 10:14:16 +0300

    task 4086

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,5 +1,13 @@
 package ru.job4j.calculator
 
+/**
+Class calculate.
+
+@author Pavel Platov (platov_p@mail.ru)
+@version $ld$
+@since 0.1
+*/
+
 public class Calculator {
 	private double result
 
@@ -7,25 +15,13 @@ public class Calculator {
 		this.result = first + second;
 	}
 
-	public double getResult() {
-		return this.result;
-	}
-
 	public void substruct(double first, double second) {
 		this.result = first - second;
-	}
-
-	public double getResult() {
-		return thes.result;
 	}
 
 	public void div(double first, double second) {
 		this.result = first / second;
 	} 
-
-	public double getResult() {
-		return thes.result;
-	}
 
 	public void multiple(double first, double second) {
 		this.result = first * second;
